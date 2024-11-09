9f5f9055b2bdd4e3e918192cba4ffaed109eb67e
dima <chocolado4kaa12@gmail.com>
2024-11-10 01:30:33 +0200

    add build

--- src/components/Loading.jsx
+++
@@ -1,8 +0,0 @@
-const Loading = () => {
-    return (
-      <div className="Load-container">
-        <h1 className="S36_L46 bold italic">Loading...</h1>
-      </div>
-    );
-  }
-  export default Loading
