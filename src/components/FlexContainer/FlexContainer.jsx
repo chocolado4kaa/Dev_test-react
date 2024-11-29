3ad1f94bfcf3f670752fd97a666bd88968510e96
dima <chocolado4kaa12@gmail.com>
2024-11-29 22:55:40 +0200

    adapt

--- src/components/FlexContainer/FlexContainer.jsx
+++ src/components/FlexContainer/FlexContainer.jsx
@@ -1,4 +1,5 @@
 import classes from "./flexContainer.module.scss";
+import { Resp } from "../Components";
 
 const FlexContainer = () => {
   return (
@@ -9,15 +10,15 @@ const FlexContainer = () => {
           title="Our Company"
           source="/Dev_test-react/AboutUs/photos_right.png"
         >
-          <p className="b2">
+          <Resp Class="b2" altClass="System S12_L20" Tag="p">
             GOpure was launched by Bloc Enterprises, a specialized distribution
             & marketing company introducing consumer products distinguished by
             innovation, technology and disruptive solutions to everyday problems
             that improve the health and well-being of people and pets. The
             company strives to practice sustainability principles in all
             operations and be a good steward of the earth.
-          </p>
-          <p className="b2">
+          </Resp>
+          <Resp Class="b2" altClass="System S12_L20" Tag="p">
             Like many amazing innovations, the inspiration for the GOpure Pod
             happened the instant its inventor in Ireland was determined to find
             a better solution after removing many dozens of used plastic water
@@ -27,14 +28,14 @@ const FlexContainer = () => {
             College in Dublin, to help realize his vision of introducing a
             portable water purifier for everyday use and eliminate the need for
             single use plastic water bottles.
-          </p>
+          </Resp>
         </FlexWrap>
         <FlexWrap
           className={classes["row-reverse"]}
           title="About PuriBloc"
           source="/Dev_test-react/AboutUs/photos_left.png"
         >
-          <p className="b2">
+          <Resp Class="b2" altClass="System S12_L20" Tag="p">
             PuriBloc is the innovative water purification, preservation
             technology inside GOpure, a portable, environmentally friendly Pod
             that keeps drinking water clean and safe, while also greatly
@@ -44,22 +45,22 @@ const FlexContainer = () => {
             technology works continuously through a natural process known as
             ionic exchange and keeps pure water fresh and great tasting for
             months.
-          </p>
-          <p className="b2">
+          </Resp>
+          <Resp Class="b2" altClass="System S12_L20" Tag="p">
             The adsorptive power of GOpure’s advanced microporous ceramic core
             is mind-boggling due to the enormous surface area for its volume
             with which water can react. This high porosity surface area carries
             millions of negatively charged binding sites, which attract and
             adsorb heavy metals along with other chemical or organic impurities,
             including bacteria.
-          </p>
-          <p className="b2">
+          </Resp>
+          <Resp Class="b2" altClass="System S12_L20" Tag="p">
             The GOpure Pod is changing the way the world drinks water by
             continually purifying and enhancing your drinking water on the go
             while also helping to save our planet: using one GOpure Pod in a
             personal re-usable bottle replaces 2,000 single-use water bottles
             with zero plastic waste.
-          </p>
+          </Resp>
         </FlexWrap>
       </div>
     </section>
@@ -80,7 +81,7 @@ const TextBlock = ({ title, children }) => {
   return (
     <div className={classes.TextBlock}>
       <div className={classes.title}>
-        <h2 className="Headline">{title}</h2>
+        <Resp Tag="h2" Class="Headline" altClass="Headline S24_L32">{title}</Resp>
       </div>
       <div className={classes.content}>{children}</div>
     </div>
